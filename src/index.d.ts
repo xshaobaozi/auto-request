43f636d88927bf2c119114b7e350d8a439e15dbc
彭毅俊 <xshaobaozi@hotmail.com>
2021-08-06 17:12:30 +0800

    doc 更新文档和用例

--- src/index.d.ts
+++
@@ -1,9 +0,0 @@
-
-interface createApiParams {
-    sourceFile: string;
-    outputPath: string;
-    options: {
-        apiDeclare: string;
-    }
-}
-declare const createApi = (sourceFile: string, outputPath: string, options: createApiParams): void => { }
--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,7 +1,2 @@
 declare module 'swagger-json-schema-request' {
-    interface Options {
-        apiPath: string;
-        apiDeclare: string;
-    }
-    export default function (sourceFile: string, { apiPath, apiDeclare }: Options): void
 }
